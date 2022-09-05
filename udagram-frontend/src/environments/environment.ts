e997b206da9012b36ded20f71914c37520850f11
anptt <phamtruongthienan@gmail.com>
2022-09-05 17:28:43 +0700

    update3

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a402c0058237b48658a48bba26aa263a-870452352.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*

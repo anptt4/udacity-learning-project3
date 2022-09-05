2c014caea6af2a018dd0b9af345716e3465f5c48
anptt <phamtruongthienan@gmail.com>
2022-09-05 17:30:39 +0700

    update 3

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,5 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=12345678
 export POSTGRES_HOST=database-1.cuv2rnclyij6.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=s3-proj3
@@ -9,3 +7,4 @@ export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8080
+kubectl get deployments

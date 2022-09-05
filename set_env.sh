77c2652dadcf83c95206e92a5a761111d5849d59
anptt <phamtruongthienan@gmail.com>
2022-09-05 16:11:00 +0700

    update

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,15 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=database-1.cuv2rnclyij6.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=s3-proj3
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
-export URL=http://localhost:8100
+export URL=http://localhost:8080
+
+export AWS_ACCESS_KEY_ID=AKIAXMAZGDQPILOLFUCO
+export AWS_SECRET_ACCESS_KEY=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
+
